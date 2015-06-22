3188bf10394c90c0337e8eff53a991403ef58236
Pyfisch <pyfisch@gmail.com>
2015-06-22 19:06:17 +0200

    docs(all): add docs for all items

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//#![deny(missing_docs)]
+#![deny(missing_docs)]
 
 //! Language tags can be used identify human languages, scripts e.g. Latin script, countries and
 //! other regions.
@@ -7,15 +7,50 @@
 //! ["Language tags in HTML and XML"](http://www.w3.org/International/articles/language-tags/) by
 //! the W3C. They are commonly used in HTML and HTTP `Content-Language` and `Accept-Language`
 //! header fields.
+//!
+//! # Examples
+//! Create a simple language tag representing the French language as spoken
+//! in Belgium and print it:
+//!
+//! ```rust
+//! use language_tags::LanguageTag;
+//! let mut langtag: LanguageTag = Default::default();
+//! langtag.language = Some("fr".to_owned());
+//! langtag.region = Some("BE".to_owned());
+//! assert_eq!(format!("{}", langtag), "fr-BE");
+//! ```
+//!
+//! Parse a tag representing a special type of English specified by private agreement:
+//!
+//! ```rust
+//! use language_tags::LanguageTag;
+//! let langtag: LanguageTag = "en-x-twain".parse().unwrap();
+//! assert_eq!(format!("{}", langtag.language.unwrap()), "en");
+//! assert_eq!(format!("{:?}", langtag.privateuse), "[\"twain\"]");
+//! ```
+//!
+//! You can check for equality, but more often you should test if two tags match.
+//! In this example we check if the resource in German language is suitable for
+//! a user from Austria. While people speaking Austrian German normally understand
+//! standard German the opposite is not always true. So the resource can be presented
+//! to the user but if the resource was in `de-AT` and a user asked for a representation
+//! in `de` the request should be rejected.
+//!
+//! ```rust
+//! use language_tags::LanguageTag;
+//! let mut langtag1: LanguageTag = Default::default();
+//! langtag1.language = Some("de".to_owned());
+//! langtag1.region = Some("AT".to_owned());
+//! let mut langtag2: LanguageTag = Default::default();
+//! langtag2.language = Some("de".to_owned());
+//! assert!(langtag2.matches(&langtag1));
+//! ```
 
 use std::fmt::{self, Display, Formatter};
 use std::str::FromStr;
 use std::default::Default;
 use std::ascii::AsciiExt;
 use std::collections::BTreeMap;
-
-#[derive(Debug, Eq, PartialEq)]
-pub struct Error;
 
 fn is_alphabetic(s: &str) -> bool {
     s.chars().all(|x| x >= 'A' && x <= 'Z' || x >= 'a' && x <= 'z')
@@ -29,8 +64,15 @@ fn is_alphanumeric_or_dash(s: &str) -> bool {
     s.chars().all(|x| x >= 'A' && x <= 'Z' || x >= 'a' && x <= 'z' || x >= '0' && x <= '9' || x == '-')
 }
 
+/// Defines an Error type for langtags.
+// TODO: Extend this to be more specific.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Error;
+
+/// Result type used for this library.
 pub type Result<T> = ::std::result::Result<T, Error>;
 
+/// Contains the 17 irregular old language tags not matching the standard grammer of tags.
 pub const GRANDFATHERED_IRREGULAR: [&'static str; 17] = [
     "en-GB-oed",
     "i-ami",
@@ -50,6 +92,7 @@ pub const GRANDFATHERED_IRREGULAR: [&'static str; 17] = [
     "sgn-BE-NL",
     "sgn-CH-DE"];
 
+/// Contains the 9 regular grandfathered tags having special semantics.
 pub const GRANDFATHERED_REGULAR: [&'static str; 9] = [
     "art-lojban",
     "cel-gaulish",
@@ -61,21 +104,52 @@ pub const GRANDFATHERED_REGULAR: [&'static str; 9] = [
     "zh-min-nan",
     "zh-xiang"];
 
-/// A language tag as desribed in [BCP47](http://tools.ietf.org/html/bcp47)
+/// A language tag as described in [BCP47](http://tools.ietf.org/html/bcp47).
+///
+/// Language tags are used to help identify languages, whether spoken,
+/// written, signed, or otherwise signaled, for the purpose of
+/// communication.  This includes constructed and artificial languages
+/// but excludes languages not intended primarily for human
+/// communication, such as programming languages.
 #[derive(Debug, Eq, Clone)]
 pub struct LanguageTag {
+    /// Language subtags are used to indicate the language, ignoring all
+    /// other aspects such as script, region or spefic invariants.
     pub language: Option<String>,
+    /// Extended language subtags are used to identify certain specially
+    /// selected languages that, for various historical and compatibility
+    /// reasons, are closely identified with or tagged using an existing
+    /// primary language subtag.
     pub extlang: Option<String>,
+    /// Script subtags are used to indicate the script or writing system
+    /// variations that distinguish the written forms of a language or its
+    /// dialects.
     pub script: Option<String>,
+    /// Region subtags are used to indicate linguistic variations associated
+    /// with or appropriate to a specific country, territory, or region.
+    /// Typically, a region subtag is used to indicate variations such as
+    /// regional dialects or usage, or region-specific spelling conventions.
+    /// It can also be used to indicate that content is expressed in a way
+    /// that is appropriate for use throughout a region, for instance,
+    /// Spanish content tailored to be useful throughout Latin America.
     pub region: Option<String>,
+    /// Variant subtags are used to indicate additional, well-recognized
+    /// variations that define a language or its dialects that are not
+    /// covered by other available subtags.
     pub variants: Vec<String>,
+    /// Extensions provide a mechanism for extending language tags for use in
+    /// various applications.  They are intended to identify information that
+    /// is commonly used in association with languages or language tags but
+    /// that is not part of language identification.
     pub extensions: BTreeMap<u8, Vec<String>>,
+    /// Private use subtags are used to indicate distinctions in language
+    /// that are important in a given context by private agreement.
     pub privateuse: Vec<String>
 }
 
 impl LanguageTag {
     /// Matches language tags like described in
-    /// [rfc4647#Extended filtering](https://tools.ietf.org/html/rfc4647#section-3.3.2)
+    /// [RFC4647#Extended filtering](https://tools.ietf.org/html/rfc4647#section-3.3.2)
     ///
     /// For example `en-GB` matches only `en-GB` and `en-Arab-GB` but not `en`. While `en` matches
     /// all of `en`, `en-GB` ,`en-Arab` and `en-Arab-GB`.
@@ -250,7 +324,6 @@ impl fmt::Display for LanguageTag {
             let mut key = String::new();
             key.push(*raw_key as char);
             try!(write!(f, "-{}", key));
-            //parts.push(key);
             for value in values {
                 try!(write!(f, "-{}", value));
             }
